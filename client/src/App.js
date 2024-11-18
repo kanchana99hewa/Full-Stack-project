d42968074e036b88dd8abdf01ea50034a08e47de
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-18 22:48:12 +0530

    container

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme } from "./utils/Themes";
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
