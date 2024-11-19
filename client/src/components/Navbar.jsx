7ec339983904bb369cd4616f584b537a353eed34
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-19 23:18:13 +0530

    button container

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 import styled from "styled-components";
 import LogoImg from "../utils/Images/Logo.png";
 import { NavLink } from "react-router-dom";
 import Button from "./Button";
+import {
+  FavoriteBorder,
+  MenuRounded,
+  SearchRounded,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+import { Avatar } from "@mui/material";
+import { logout } from "../redux/reducers/userSlice";
+import { useDispatch } from "react-redux";
 
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+`;
+const NavbarContainer = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 0 24px;
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
 const NavLogo = styled.div`
   width: 100%;
   display: flex;
@@ -14,7 +45,6 @@ const NavLogo = styled.div`
   text-decoration: none;
   color: inherit;
 `;
-
 const Logo = styled.img`
   height: 34px;
 `;
@@ -26,6 +56,36 @@ const NavItems = styled.ul`
   gap: 32px;
   padding: 0 6px;
   list-style: none;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const Navlink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 1s slide-in;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.active {
+    color: ${({ theme }) => theme.primary};
+    border-bottom: 1.8px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 28px;
+  align-items: center;
+  padding: 0 6px;
+  color: ${({ theme }) => theme.primary};
   @media screen and (max-width: 768px) {
     display: none;
   }
