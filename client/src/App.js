849e611a04cbd4581a7c0f28c1d3cc7b81074a0f
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-20 22:39:34 +0530

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ShopListing from "./pages/ShopListing";
+import Favourite from "./pages/Favourite";
+import Cart from "./pages/Cart";
+import ProductDetails from "./pages/ProductDetails";
 import { lightTheme } from "./utils/Themes";
 
 const Container = styled.div`
@@ -14,21 +22,24 @@ const Container = styled.div`
 `;
 
 function App() {
+  const [openAuth, setOpenAuth] = useState(false); // Tracks auth modal state
+  const currentUser = null; // Mocked user state; replace with actual logic
+
   return (
     <ThemeProvider theme={lightTheme}>
-<BrowserRouter>
-          <Container>
+      <BrowserRouter>
+        <Container>
           <Navbar setOpenAuth={setOpenAuth} currentUser={currentUser} />
           <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/shop" exact element={<ShopListing />} />
-            <Route path="/favorite" exact element={<Favourite />} />
-            <Route path="/cart" exact element={<Cart />} />
-            <Route path="/shop/:id" exact element={<ProductDetails />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<ShopListing />} />
+            <Route path="/favorite" element={<Favourite />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/shop/:id" element={<ProductDetails />} />
           </Routes>
-          </Container>
-</BrowserRouter>
-      </ThemeProvider>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ import {
 } from "@mui/icons-material";
 import { Avatar } from "@mui/material";
 import { logout } from "../redux/reducers/userSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Nav = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -25,6 +25,7 @@ const Nav = styled.div`
   z-index: 10;
   color: white;
 `;
+
 const NavbarContainer = styled.div`
   width: 100%;
   max-width: 1400px;
@@ -35,8 +36,8 @@ const NavbarContainer = styled.div`
   justify-content: space-between;
   font-size: 1rem;
 `;
+
 const NavLogo = styled.div`
-  width: 100%;
   display: flex;
   align-items: center;
   padding: 0 6px;
@@ -45,63 +46,91 @@ const NavLogo = styled.div`
   text-decoration: none;
   color: inherit;
 `;
+
 const Logo = styled.img`
   height: 34px;
 `;
+
 const NavItems = styled.ul`
-  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 32px;
-  padding: 0 6px;
   list-style: none;
+
   @media screen and (max-width: 768px) {
     display: none;
   }
 `;
+
 const Navlink = styled(NavLink)`
-  display: flex;
-  align-items: center;
   color: ${({ theme }) => theme.text_primary};
   font-weight: 500;
-  cursor: pointer;
-  transition: all 1s slide-in;
   text-decoration: none;
+
   &:hover {
     color: ${({ theme }) => theme.primary};
   }
+
   &.active {
     color: ${({ theme }) => theme.primary};
     border-bottom: 1.8px solid ${({ theme }) => theme.primary};
   }
 `;
 
-const ButtonContainer = styled.div`
+const MobileIcon = styled.div`
+  display: none;
+  cursor: pointer;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 80px;
+  right: 0;
   width: 100%;
-  height: 100%;
+  background-color: ${({ theme }) => theme.bg};
+  z-index: 5;
+
+  a {
+    padding: 16px;
+    text-align: center;
+    text-decoration: none;
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
+
+const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
   gap: 28px;
   align-items: center;
-  padding: 0 6px;
-  color: ${({ theme }) => theme.primary};
+
   @media screen and (max-width: 768px) {
     display: none;
   }
 `;
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user.currentUser);
+
   return (
     <Nav>
-    <NavbarContainer>
-      <MobileIcon onClick={() => setIsOpen(!isOpen)}>
-        <MenuRounded style={{ color: "inherit" }} />
-      </MobileIcon>
-      <NavLogo>
+      <NavbarContainer>
+        <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+          <MenuRounded style={{ color: "inherit" }} />
+        </MobileIcon>
+        <NavLogo>
           <Logo src={LogoImg} />
         </NavLogo>
-        
+
         <NavItems>
           <Navlink to="/">Home</Navlink>
           <Navlink to="/Shop">Shop</Navlink>
@@ -109,31 +138,34 @@ const Navbar = () => {
           <Navlink to="/Orders">Orders</Navlink>
           <Navlink to="/Contact">Contact</Navlink>
         </NavItems>
-        
+
         {isOpen && (
-          <MobileMenu isOpen={isOpen}>
-            <Navlink to="/" onClick={() => setIsOpen(!isOpen)}>
+          <MobileMenu>
+            <Navlink to="/" onClick={() => setIsOpen(false)}>
               Home
             </Navlink>
-            <Navlink onClick={() => setIsOpen(!isOpen)} to="/Shop">
+            <Navlink to="/Shop" onClick={() => setIsOpen(false)}>
               Shop
             </Navlink>
-            <Navlink onClick={() => setIsOpen(!isOpen)} to="/New_Arrivals">
+            <Navlink to="/New_Arrivals" onClick={() => setIsOpen(false)}>
               New Arrivals
             </Navlink>
-            <Navlink onClick={() => setIsOpen(!isOpen)} to="/Orders">
+            <Navlink to="/Orders" onClick={() => setIsOpen(false)}>
               Orders
             </Navlink>
-            <Navlink onClick={() => setIsOpen(!isOpen)} to="/Contact">
+            <Navlink to="/Contact" onClick={() => setIsOpen(false)}>
               Contact
             </Navlink>
             {currentUser ? (
               <Button text="Logout" small onClick={() => dispatch(logout())} />
             ) : (
-
+              <Button text="Login" small />
+            )}
+          </MobileMenu>
+        )}
       </NavbarContainer>
     </Nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
