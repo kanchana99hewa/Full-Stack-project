459e49934c85b5d8914710894ef80dca56d2b14c
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-20 23:47:34 +0530

    authentication

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Authentication = () => {
   return (
-    <div>Authentication</div>
+    <Modal>
+      <Container>
+      Authentication
+      </Container>
+      </Modal>
   )
 }
 
