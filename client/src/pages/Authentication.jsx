95aca24940a2c940812655f8e1f5e1266ad5e5fa
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-21 22:50:10 +0530

    left

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Modal } from "@mui/material";
+import styled from "styled-components";
 
-
+const Left = styled.div`
+  flex: 1;
+  position: relative;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const Authentication = ({ openAuth, setOpenAuth }) => {
   return (
