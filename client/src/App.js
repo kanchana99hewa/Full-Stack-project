0df9bffd4f4be10224f1abbf2142cae7147ae43e
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-21 22:46:05 +0530

    openauth

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { useState } from "react";
+import Authentication from "./pages/Authentication";
 import ShopListing from "./pages/ShopListing";
 import Favourite from "./pages/Favourite";
 import Cart from "./pages/Cart";
 import ProductDetails from "./pages/ProductDetails";
-import { lightTheme } from "./utils/Themes";
+import { useDispatch, useSelector } from "react-redux";
+import ToastMessage from "./components/ToastMessage";
 
 const Container = styled.div`
   width: 100%;
@@ -22,21 +25,25 @@ const Container = styled.div`
 `;
 
 function App() {
-  const [openAuth, setOpenAuth] = useState(false); // Tracks auth modal state
-  const currentUser = null; // Mocked user state; replace with actual logic
-
+  const { currentUser } = useSelector((state) => state.user);
+  const { open, message, severity } = useSelector((state) => state.user);
+  const [openAuth, setOpenAuth] = useState(false);
   return (
     <ThemeProvider theme={lightTheme}>
       <BrowserRouter>
         <Container>
           <Navbar setOpenAuth={setOpenAuth} currentUser={currentUser} />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<ShopListing />} />
-            <Route path="/favorite" element={<Favourite />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/shop/:id" element={<ProductDetails />} />
+            <Route path="/" exact element={<Home />} />
+            <Route path="/shop" exact element={<ShopListing />} />
+            <Route path="/favorite" exact element={<Favourite />} />
+            <Route path="/cart" exact element={<Cart />} />
+            <Route path="/shop/:id" exact element={<ProductDetails />} />
           </Routes>
+          {openAuth && (
+            <Authentication openAuth={openAuth} setOpenAuth={setOpenAuth} />
+          )}
+         
         </Container>
       </BrowserRouter>
     </ThemeProvider>
