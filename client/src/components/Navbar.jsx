028098e006de3b1420e4835a919918599a518ce6
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-23 22:06:41 +0530

    menu icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import styled from "styled-components"; 
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuRounded from "@mui/icons-material/MenuRounded";
+import LogoImg from "../utils/Images/Logo.png"; // Ensure this path is correct
 
 const NavbarContainer = styled.div`
   width: 100%;
@@ -12,12 +14,50 @@ const NavbarContainer = styled.div`
   font-size: 1rem;
 `;
 
+const MobileIcon = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Mobileicons = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+  }
+`;
+
+const NavLogo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 50px;
+  height: auto;
+`;
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav>
       <h1>Navbar</h1>
       <NavbarContainer>
-        {/* Navbar content goes here */}
+        <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+          <MenuRounded style={{ color: "inherit" }} />
+        </MobileIcon>
+
+        <NavLogo>
+          <Logo src={LogoImg} alt="Logo" />
+        </NavLogo>
       </NavbarContainer>
     </nav>
   );
