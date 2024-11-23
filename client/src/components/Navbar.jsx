a7b01cf5647111c814d7d021155c1392a799126d
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-23 22:12:07 +0530

    nav items

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import MenuRounded from "@mui/icons-material/MenuRounded";
-import LogoImg from "../utils/Images/Logo.png"; // Ensure this path is correct
+import LogoImg from "../utils/Images/Logo.png";
+import { NavLink } from "react-router-dom";
+import { MenuRounded } from "@mui/icons-material";
 
 const NavbarContainer = styled.div`
   width: 100%;
@@ -23,17 +24,6 @@ const MobileIcon = styled.div`
   }
 `;
 
-const Mobileicons = styled.div`
-  color: ${({ theme }) => theme.text_primary};
-  display: none;
-  @media screen and (max-width: 768px) {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 16px;
-  }
-`;
-
 const NavLogo = styled.div`
   display: flex;
   align-items: center;
@@ -44,12 +34,41 @@ const Logo = styled.img`
   height: auto;
 `;
 
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Navlink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.active {
+    color: ${({ theme }) => theme.primary};
+    border-bottom: 1.8px solid ${({ theme }) => theme.primary};
+  }
+`;
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav>
-      <h1>Navbar</h1>
       <NavbarContainer>
         <MobileIcon onClick={() => setIsOpen(!isOpen)}>
           <MenuRounded style={{ color: "inherit" }} />
@@ -58,6 +77,14 @@ const Navbar = () => {
         <NavLogo>
           <Logo src={LogoImg} alt="Logo" />
         </NavLogo>
+
+        <NavItems>
+          <Navlink to="/">Home</Navlink>
+          <Navlink to="/Shop">Shop</Navlink>
+          <Navlink to="/New_Arrivals">New Arrivals</Navlink>
+          <Navlink to="/Orders">Orders</Navlink>
+          <Navlink to="/Contact">Contact</Navlink>
+        </NavItems>
       </NavbarContainer>
     </nav>
   );
