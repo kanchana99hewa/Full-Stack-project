6bd9cb5b72a90d1ef5bcfcd5c2d850d101d982a9
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-23 21:55:04 +0530

    navbar container

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components"; 
 
+const NavbarContainer = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 0 24px;
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
 
 const Navbar = () => {
   return (
     <nav>
-    Navbar 
+      <h1>Navbar</h1>
+      <NavbarContainer>
+        {/* Navbar content goes here */}
+      </NavbarContainer>
     </nav>
   );
 };
 
-export default Navbar; 
+export default Navbar;
