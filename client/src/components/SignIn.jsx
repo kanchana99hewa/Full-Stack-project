dfca91dda8e6b5bdd18e5bcf81ec236188bb31eb
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-24 21:51:45 +0530

    text input

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import TextInput from "./TextInput";
 
 
 
@@ -10,16 +11,42 @@ const Container = styled.div`
   flex-direction: column;
   gap: 36px;
 `;
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.primary};
+`;
+const Span = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 90};
+`;
 
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Container>
       <div>
       <Title>Welcome to Krist 👋</Title>
-      <Textnput>
-
-      </Textnput>
+      <Span>Please login with your details here</Span>
       </div>
+      <TextInput
+          label="Email Address"
+          placeholder="Enter your email address"
+          value={email}
+          handelChange={(e) => setEmail(e.target.value)}
+        />
+        <TextInput
+          label="Password"
+          placeholder="Enter your password"
+          password
+          value={password}
+          handelChange={(e) => setPassword(e.target.value)}
+        />
+   
+      
       
     </Container>
   )
