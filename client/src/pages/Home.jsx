4ca61a8d9eafa8c5360f32a8fb6b1ae487631e29
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-25 22:23:45 +0530

    container

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+  @media (max-width: 768px) {
+    padding: 20px 12px;
+  }
+  background: ${({ theme }) => theme.bg};
+`;
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        This is Home
-      </div>
-  
-    </div>
+    <Container>
+      <Section>
+
+      </Section>
+      </Container>
   );
 }
 
