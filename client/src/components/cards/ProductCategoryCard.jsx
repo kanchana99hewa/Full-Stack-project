bfa5a6abf3ef7b1865bb8257eed0890de81e46db
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-26 23:04:14 +0530

    card

--- client/src/components/cards/ProductCategoryCard.jsx
+++ client/src/components/cards/ProductCategoryCard.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-function ProductCategoryCard() {
+
+const Card = styled.div`
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  transition: all 0.3s ease-out;
+  cursor: pointer;
+  @media (max-width: 600px) {
+    width: 170px;
+  }
+`;
+
+const ProductCategoryCard = ({ category }) => {
+    const navigate = useNavigate();
+
   return (
     <div>
+      <Card onClick={() => navigate(`/shop?category=${category.name}`)}>
       
+      </Card>
     </div>
   )
 }
