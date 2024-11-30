3fd9284df0d89840b5df7e82fd7004ff604e751a
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-30 23:39:59 +0530

    menu

--- client/src/components/cards/ProductCard.jsx
+++ client/src/components/cards/ProductCard.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+const Menu = styled.div`
+  position: absolute;
+  z-index: 10;
+  color: ${({ theme }) => theme.text_primary};
+  top: 14px;
+  right: 14px;
+  display: none;
+  flex-direction: column;
+  gap: 12px;
+`;
+
 function ProductCard() {
   return (
     <Card>
