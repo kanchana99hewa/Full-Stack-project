39866ea6d754311d117416d474e90b18b77d19ff
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-11-30 23:40:52 +0530

    card

--- client/src/components/cards/ProductCard.jsx
+++ client/src/components/cards/ProductCard.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  transition: all 0.3s ease-out;
+  cursor: pointer;
+  @media (max-width: 600px) {
+    width: 170px;
+  }
+`;
 
 const Menu = styled.div`
   position: absolute;
