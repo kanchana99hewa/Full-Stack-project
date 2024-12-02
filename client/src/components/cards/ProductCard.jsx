f3d39f188c3af0ce65650c7a7c755dae8e852cc7
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-02 22:50:23 +0530

    product card

--- client/src/components/cards/ProductCard.jsx
+++ client/src/components/cards/ProductCard.jsx
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { CircularProgress, Rating } from "@mui/material";
+import styled from "styled-components";
+import {
+  AddShoppingCartOutlined,
+  FavoriteBorder,
+  FavoriteRounded,
+} from "@mui/icons-material";
+import {
+  addToCart,
+  addToFavourite,
+  deleteFromFavourite,
+  getFavourite,
+} from "../../api";
+import { useDispatch } from "react-redux";
+import { openSnackbar } from "../../redux/reducers/snackbarSlice";
 
 const Card = styled.div`
   width: 250px;
@@ -13,17 +27,6 @@ const Card = styled.div`
     width: 170px;
   }
 `;
-
-const Menu = styled.div`
-  position: absolute;
-  z-index: 10;
-  color: ${({ theme }) => theme.text_primary};
-  top: 14px;
-  right: 14px;
-  display: none;
-  flex-direction: column;
-  gap: 12px;
-`;
 const Image = styled.img`
   width: 100%;
   height: 320px;
@@ -34,13 +37,34 @@ const Image = styled.img`
     height: 240px;
   }
 `;
-const Details = styled.div`
-import { useDispatch } from "react-redux";
-import { openSnackbar } from "../../redux/reducers/snackbarSlice";
-  display: flex;
-  gap: 6px;
+const Menu = styled.div`
+  position: absolute;
+  z-index: 10;
+  color: ${({ theme }) => theme.text_primary};
+  top: 14px;
+  right: 14px;
+  display: none;
   flex-direction: column;
-  padding: 4px 10px;
+  gap: 12px;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  border-radius: 6px;
+  transition: all 0.3s ease-out;
+  &:hover {
+    background-color: ${({ theme }) => theme.primary};
+  }
+
+  &:hover ${Image} {
+    opacity: 0.9;
+  }
+  &:hover ${Menu} {
+    display: flex;
+  }
 `;
 const MenuItem = styled.div`
   border-radius: 50%;
@@ -53,89 +77,151 @@ const MenuItem = styled.div`
   justify-content: center;
   z-index: 200;
 `;
-const addFavorite = async () => {
-  setFavoriteLoading(true);
-  const token = localStorage.getItem("krist-app-token");
-  await addToFavourite(token, { productID: product?._id })
-    .then((res) => {
-      setFavorite(true);
-      setFavoriteLoading(false);
-    })
-    .catch((err) => {
-      setFavoriteLoading(false);
-      dispatch(
-        openSnackbar({
-          message: err.message,
-          severity: "error",
-        })
-      );
-    });
-};
-const removeFavorite = async () => {
-  setFavoriteLoading(true);
-  const token = localStorage.getItem("krist-app-token");
-  await deleteFromFavourite(token, { productID: product?._id })
-    .then((res) => {
-      setFavorite(false);
-      setFavoriteLoading(false);
-    })
-    .catch((err) => {
-      setFavoriteLoading(false);
-      dispatch(
-        openSnackbar({
-          message: err.message,
-          severity: "error",
-        })
-      );
-    });
-};
-const addCart = async () => {
-  const token = localStorage.getItem("krist-app-token");
-  await addToCart(token, { productId: product?._id, quantity: 1 })
-    .then((res) => {
-      navigate("/cart");
-    })
-    .catch((err) => {
-      dispatch(
-        openSnackbar({
-          message: err.message,
-          severity: "error",
-        })
-      );
-    });
-};
-const checkFavourite = async () => {
-  setFavoriteLoading(true);
-  const token = localStorage.getItem("krist-app-token");
-  await getFavourite(token, { productId: product?._id })
-    .then((res) => {
-      const isFavorite = res.data?.some(
-        (favorite) => favorite._id === product?._id
-      );
-      setFavorite(isFavorite);
-      setFavoriteLoading(false);
-    })
-    .catch((err) => {
-      setFavoriteLoading(false);
-      dispatch(
-        openSnackbar({
-          message: err.message,
-          severity: "error",
-        })
-      );
-    });
-};
-useEffect(() => {
-  checkFavourite();
-}, []);
 
+const Rate = styled.div`
+  position: absolute;
+  z-index: 10;
+  color: ${({ theme }) => theme.text_primary};
+  bottom: 8px;
+  left: 8px;
+  padding: 4px 8px;
+  border-radius: 4px;
+  background: white;
+  display: flex;
+  align-items: center;
+  opacity: 0.9;
+`;
+
+const Details = styled.div`
+import { useDispatch } from "react-redux";
+import { openSnackbar } from "../../redux/reducers/snackbarSlice";
+  display: flex;
+  gap: 6px;
+  flex-direction: column;
+  padding: 4px 10px;
+`;
+const Title = styled.div`
+  font-size: 16px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text_primary};
+`;
+const Desc = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+const Price = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 18px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+`;
+const Span = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_secondary + 60};
+  text-decoration: line-through;
+  text-decoration-color: ${({ theme }) => theme.text_secondary + 50};
+`;
+const Percent = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  color: green;
+`;
+
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [favorite, setFavorite] = useState(false);
+  const [favoriteLoading, setFavoriteLoading] = useState(false);
+
+  const addFavorite = async () => {
+    setFavoriteLoading(true);
+    const token = localStorage.getItem("krist-app-token");
+    await addToFavourite(token, { productID: product?._id })
+      .then((res) => {
+        setFavorite(true);
+        setFavoriteLoading(false);
+      })
+      .catch((err) => {
+        setFavoriteLoading(false);
+        dispatch(
+          openSnackbar({
+            message: err.message,
+            severity: "error",
+          })
+        );
+      });
+  };
+  const removeFavorite = async () => {
+    setFavoriteLoading(true);
+    const token = localStorage.getItem("krist-app-token");
+    await deleteFromFavourite(token, { productID: product?._id })
+      .then((res) => {
+        setFavorite(false);
+        setFavoriteLoading(false);
+      })
+      .catch((err) => {
+        setFavoriteLoading(false);
+        dispatch(
+          openSnackbar({
+            message: err.message,
+            severity: "error",
+          })
+        );
+      });
+  };
+  const addCart = async () => {
+    const token = localStorage.getItem("krist-app-token");
+    await addToCart(token, { productId: product?._id, quantity: 1 })
+      .then((res) => {
+        navigate("/cart");
+      })
+      .catch((err) => {
+        dispatch(
+          openSnackbar({
+            message: err.message,
+            severity: "error",
+          })
+        );
+      });
+  };
+  const checkFavourite = async () => {
+    setFavoriteLoading(true);
+    const token = localStorage.getItem("krist-app-token");
+    await getFavourite(token, { productId: product?._id })
+      .then((res) => {
+        const isFavorite = res.data?.some(
+          (favorite) => favorite._id === product?._id
+        );
+        setFavorite(isFavorite);
+        setFavoriteLoading(false);
+      })
+      .catch((err) => {
+        setFavoriteLoading(false);
+        dispatch(
+          openSnackbar({
+            message: err.message,
+            severity: "error",
+          })
+        );
+      });
+  };
+
+  useEffect(() => {
+    checkFavourite();
+  }, []);
   return (
     <Card>
       <Top>
-      <Image src={product?.img} />
+        <Image src={product?.img} />
         <Menu>
-
-        <MenuItem
+          <MenuItem
             onClick={() => (favorite ? removeFavorite() : addFavorite())}
           >
             {favoriteLoading ? (
@@ -155,29 +241,21 @@ useEffect(() => {
               sx={{ color: "inherit", fontSize: "20px" }}
             />
           </MenuItem>
-          <MenuItem>
-             <Rate>
+        </Menu>
+        <Rate>
           <Rating value={3.5} sx={{ fontSize: "14px" }} />
-            </Rate>
-            <Details onClick={() => navigate(`/shop/${product._id}`)}>
-            <Title>{product?.title}</Title>
-            <Desc>{product?.name}</Desc>
-            <Price>
-              ${product?.price?.org} <Span>${product?.price?.mrp}</Span>
-              <Percent>${product?.price?.off}% Off</Percent>
-            </Price>
-           </Details>
-        <Price>
-            <Title>
-
-            </Title>
-        </Price>
-          </MenuItem>
-          </Menu>
+        </Rate>
       </Top>
+      <Details onClick={() => navigate(`/shop/${product._id}`)}>
+        <Title>{product?.title}</Title>
+        <Desc>{product?.name}</Desc>
+        <Price>
+          ${product?.price?.org} <Span>${product?.price?.mrp}</Span>
+          <Percent>${product?.price?.off}% Off</Percent>
+        </Price>
+      </Details>
+    </Card>
+  );
+};
 
-      </Card>
-  )
-}
-
-export default ProductCard
+export default ProductCard;
