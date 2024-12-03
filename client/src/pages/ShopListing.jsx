9076f6c864124b022dcf39e59e4c1c9a5df93f2c
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-03 23:13:45 +0530

    container

--- client/src/pages/ShopListing.jsx
+++ client/src/pages/ShopListing.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
+const Container = styled.div`
+  padding: 20px 30px;
+  height: 100vh;
+  overflow-y: hidden;
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  @media (max-width: 768px) {
+    padding: 20px 12px;
+    flex-direction: column;
+    overflow-y: scroll;
+  }
+  background: ${({ theme }) => theme.bg};
+`;
+
 const ShopListing = () => {
+  const [loading, setLoading] = useState(false);
+
+
   return (
     <Container>
        {loading ? (
@@ -14,20 +32,39 @@ const ShopListing = () => {
                  <Title>{filters.name}</Title>
                   {filters.value === "price" ? (
                     <>
-                    <Slider
-                      aria-label="Price"
-                      defaultValue={priceRange}
-                      min={0}
-                      max={1000}
-                      valueLabelDisplay="auto"
-                      marks={[
-                        { value: 0, label: "$0" },
-                        { value: 1000, label: "$1000" },
-                      ]}
-                      onChange={(e, newValue) => setPriceRange(newValue)}
-                    />
-                  </>
-                ) : filt
+                <Slider
+                        aria-label="Price"
+                        defaultValue={priceRange}
+                        min={0}
+                        max={1000}
+                        valueLabelDisplay="auto"
+                        marks={[
+                          { value: 0, label: "$0" },
+                          { value: 1000, label: "$1000" },
+                        ]}
+                        onChange={(e, newValue) => setPriceRange(newValue)}
+                      />
+                    </>
+                  ) : filters.value === "size" ? (
+                    <Item>
+                    {filters.items.map((item) => (
+                      <SelectableItem
+                        key={item}
+                        selected={selectedSizes.includes(item)}
+                        onClick={() =>
+                          setSelectedSizes((prevSizes) =>
+                            prevSizes.includes(item)
+                              ? prevSizes.filter(
+                                  (category) => category !== item
+                                )
+                              : [...prevSizes, item]
+                          )
+                        }
+                      >
+                        {item}
+                      </SelectableItem>
+                    ))}
+                  </Item>
       <Container/>
   )
 }
