db95b6536a20cb110719fc4b17b5117e2e7ab81d
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-03 22:52:13 +0530

    slider

--- client/src/pages/ShopListing.jsx
+++ client/src/pages/ShopListing.jsx
@@ -13,7 +13,21 @@ const ShopListing = () => {
               <FilterSection>
                  <Title>{filters.name}</Title>
                   {filters.value === "price" ? (
-                    
+                    <>
+                    <Slider
+                      aria-label="Price"
+                      defaultValue={priceRange}
+                      min={0}
+                      max={1000}
+                      valueLabelDisplay="auto"
+                      marks={[
+                        { value: 0, label: "$0" },
+                        { value: 1000, label: "$1000" },
+                      ]}
+                      onChange={(e, newValue) => setPriceRange(newValue)}
+                    />
+                  </>
+                ) : filt
       <Container/>
   )
 }
