f2805320ef40596ddfa6fb7ce38914ebc6c046a5
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-08 23:32:43 +0530

    cart

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -26,18 +26,18 @@ const Cart = () => {
                   <Table>
                     <TableItem flex>
                       <Product>
-                      <Img src={item?.product?.img} />
+                        <Img src={item?.product?.img} />
                         <Details>
                           <Protitle>{item?.product?.title}</Protitle>
                           <ProDesc>{item?.product?.name}</ProDesc>
                           <ProSize>Size: Xl</ProSize>
                         </Details>
                       </Product>
-                      </TableItem>
+                    </TableItem>
                     <TableItem>${item?.product?.price?.org}</TableItem>
                     <TableItem>
                       <Counter>
-                      <div
+                        <div
                           style={{
                             cursor: "pointer",
                             flex: 1,
@@ -58,7 +58,12 @@ const Cart = () => {
                         >
                           +
                         </div>
-                        </TableItem>
+                      </Counter>
+                    </TableItem>
+                    <TableItem>
+                      {" "}
+                      ${(item.quantity * item?.product?.price?.org).toFixed(2)}
+                    </TableItem>
                     <TableItem>
                       <DeleteOutline
                         sx={{ color: "red" }}
@@ -71,9 +76,111 @@ const Cart = () => {
                         }
                       />
                     </TableItem>
-
-          </Section>
-          </Container>
+                  </Table>
+                ))}
+              </Left>
+              <Right>
+                <Subtotal>
+                  Subtotal : ${calculateSubtotal().toFixed(2)}
+                </Subtotal>
+                <Delivery>
+                  Delivery Details:
+                  <div>
+                    <div
+                      style={{
+                        display: "flex",
+                        gap: "6px",
+                      }}
+                    >
+                      <TextInput
+                        small
+                        placeholder="First Name"
+                        value={deliveryDetails.firstName}
+                        handelChange={(e) =>
+                          setDeliveryDetails({
+                            ...deliveryDetails,
+                            firstName: e.target.value,
+                          })
+                        }
+                      />
+                      <TextInput
+                        small
+                        placeholder="Last Name"
+                        value={deliveryDetails.lastName}
+                        handelChange={(e) =>
+                          setDeliveryDetails({
+                            ...deliveryDetails,
+                            lastName: e.target.value,
+                          })
+                        }
+                      />
+                    </div>
+                    <TextInput
+                      small
+                      value={deliveryDetails.emailAddress}
+                      handelChange={(e) =>
+                        setDeliveryDetails({
+                          ...deliveryDetails,
+                          emailAddress: e.target.value,
+                        })
+                      }
+                      placeholder="Email Address"
+                    />
+                    <TextInput
+                      small
+                      value={deliveryDetails.phoneNumber}
+                      handelChange={(e) =>
+                        setDeliveryDetails({
+                          ...deliveryDetails,
+                          phoneNumber: e.target.value,
+                        })
+                      }
+                      placeholder="Phone no. +91 XXXXX XXXXX"
+                    />
+                    <TextInput
+                      small
+                      textArea
+                      rows="5"
+                      handelChange={(e) =>
+                        setDeliveryDetails({
+                          ...deliveryDetails,
+                          completeAddress: e.target.value,
+                        })
+                      }
+                      value={deliveryDetails.completeAddress}
+                      placeholder="Complete Address (Address, State, Country, Pincode)"
+                    />
+                  </div>
+                </Delivery>
+                <Delivery>
+                  Payment Details:
+                  <div>
+                    <TextInput small placeholder="Card Number" />
+                    <div
+                      style={{
+                        display: "flex",
+                        gap: "6px",
+                      }}
+                    >
+                      <TextInput small placeholder="Expiry Date" />
+                      <TextInput small placeholder="CVV" />
+                    </div>
+                    <TextInput small placeholder="Card Holder name" />
+                  </div>
+                </Delivery>
+                <Button
+                  text="Pace Order"
+                  small
+                  isLoading={buttonLoad}
+                  isDisabled={buttonLoad}
+                  onClick={PlaceOrder}
+                />
+              </Right>
+            </Wrapper>
+          )}
+        </Section>
+      )}
+    </Container>
   )
 }
 
