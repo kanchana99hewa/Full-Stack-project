fed13f8ca19494666711d1f62c2e947472fd09ec
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-08 23:25:37 +0530

    table

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -8,6 +8,20 @@ const Cart = () => {
       ) : (
         <Section>
           <Title>Your Shopping Cart</Title>
+          {products.length === 0 ? (
+            <>Cart is empty</>
+          ) : (
+            <Wrapper>
+              <Left>
+                <Table>
+                  <TableItem bold flex>
+                    Product
+                  </TableItem>
+                  <TableItem bold>Price</TableItem>
+                  <TableItem bold>Quantity</TableItem>
+                  <TableItem bold>Subtotal</TableItem>
+                  <TableItem></TableItem>
+                </Table>
           </Section>
           </Container>
   )
