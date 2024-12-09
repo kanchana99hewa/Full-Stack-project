f366e5c5ca36e5237a8ff6221674dc7785081245
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-09 22:46:52 +0530

    container

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+  @media (max-width: 768px) {
+    padding: 20px 12px;
+  }
+  background: ${({ theme }) => theme.bg};
+`;
+
 const Cart = () => {
   return (
     <Container>
