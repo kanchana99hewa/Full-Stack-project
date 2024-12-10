d485b556b4da0e7126e833f6e16eb0208423c9be
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-10 21:34:10 +0530

    container

--- client/src/pages/Favourite.jsx
+++ client/src/pages/Favourite.jsx
@@ -1,4 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { CircularProgress } from "@mui/material";
+
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+  @media (max-width: 768px) {
+    padding: 20px 12px;
+  }
+  background: ${({ theme }) => theme.bg};
+`;
 
 const Favourite = () => {
   return (
