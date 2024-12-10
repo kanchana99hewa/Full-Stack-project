f2951c2be45fc5b5cd42e2a6a24bd1e3acf866c3
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-10 21:30:05 +0530

    circular process

--- client/src/pages/Favourite.jsx
+++ client/src/pages/Favourite.jsx
@@ -6,9 +6,24 @@ const Favourite = () => {
     <Section>
       <Title>Your favourites</Title>
       <CardWrapper>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <>
+            {products.length === 0 ? (
+              <>No Products</>
+            ) : (
+              <CardWrapper>
+                {products.map((product) => (
+                  <ProductCard product={product} />
+                ))}
+              </CardWrapper>
+            )}
+          </>
+        )}
       </CardWrapper>
-      </Section>
-    </Container>
+    </Section>
+  </Container>
   )
 }
 
