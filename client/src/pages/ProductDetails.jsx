b37a5e183daf30fba39efd4e3d934975253bbe74
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-11 22:41:30 +0530

    price

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -8,6 +8,19 @@ function ProductDetails() {
       ) : (
         <Wrapper>
           <ImageWrapper>
+            <Image src={product?.img} />
+          </ImageWrapper>
+          <Details>
+            <div>
+              <Title>{product?.title}</Title>
+              <Name>{product?.name}</Name>
+            </div>
+            <Rating value={3.5} />
+            <Price>
+              ${product?.price?.org} <Span>${product?.price?.mrp}</Span>{" "}
+              <Percent> (${product?.price?.off}% Off) </Percent>
+            </Price>
+            <Desc>{product?.desc}</Desc>
           </Wrapper>
       )}
     </Container>
