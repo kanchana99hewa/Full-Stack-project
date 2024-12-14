375004046f5171d0d046d12e8e03ae0fd40deff1
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-14 23:54:15 +0530

    index

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-A
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
