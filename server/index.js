2ddfc20b1cc96d24d690e5dca528aedd55c6e497
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-18 23:17:16 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -13,40 +13,42 @@ app.use(express.urlencoded({ extended: true }));
 
 //error handel
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong";
-    return res.status(status).json({
-      success: false,
-      status,
-      message,
-    });
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
-    app.get("/", async (req, res) => {
-        res.status(200).json({
-          message: "Hello GFG Developers",
-        });
-      });
-      app.use("/api/user/", UserRouter);
-      app.use("/api/products/", ProductRoutes);
-    
-      const connectDB = () => {
-        mongoose.set("strictQuery", true);
-        mongoose
-          .connect(process.env.MODNO_DB)
-          .then(() => console.log("Connected to MONGO DB"))
-          .catch((err) => {
-            console.error("failed to connect with mongo");
-            console.error(err);
-          });
-      };
-      const startServer = async () => {
-        try {
-          connectDB();
-          app.listen(8080, () => console.log("Server started on port 8080"));
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      
-      startServer();
-      
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello GFG Developers",
+  });
+});
+
+app.use("/api/user/", UserRouter);
+app.use("/api/products/", ProductRoutes);
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MODNO_DB)
+    .then(() => console.log("Connected to MONGO DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
