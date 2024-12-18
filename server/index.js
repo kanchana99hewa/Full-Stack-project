d3fe7d631c0d949d3e3be5f2d1cf932bf6037dc7
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-18 23:05:04 +0530

    error handel

--- server/index.js
+++ server/index.js
@@ -2,3 +2,14 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+//error handel
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong";
+    return res.status(status).json({
+      success: false,
+      status,
+      message,
+    });
+  
