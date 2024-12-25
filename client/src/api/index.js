26669c7ab1087248109dd6c402f763929fe8dbd6
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-25 22:50:36 +0530

    baseURL

---
+++ client/src/api/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:8080/api/",
+});
+
+export const UserSignUp = async (data) => await API.post("/user/signup", data);
+export const UserSignIn = async (data) => await API.post("/user/signin", data);
