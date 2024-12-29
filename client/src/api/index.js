8a47e743c8c89436ef3a4b5c79d47fac802d1be1
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-29 11:48:46 +0530

    products api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,3 +6,9 @@ const API = axios.create({
 
 export const UserSignUp = async (data) => await API.post("/user/signup", data);
 export const UserSignIn = async (data) => await API.post("/user/signin", data);
+
+//Products
+export const getAllProducts = async (filter) =>
+  await API.get(`/products?${filter}`);
+
+export const getProductDetails = async (id) => await API.get(`/products/${id}`);
