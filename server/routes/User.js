688758815b2b57096194b6eee0d836fd833b627b
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-12-29 19:00:16 +0530

    jsonwebtoken

---
+++ server/routes/User.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  UserLogin,
+  UserRegister,
+  addToCart,
+  addToFavorites,
+  getAllCartItems,
+  getAllOrders,
+  getUserFavourites,
+  placeOrder,
+  removeFromCart,
+  removeFromFavorites,
+} from "../controllers/User.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.post("/signup", UserRegister);
+router.post("/signin", UserLogin);
