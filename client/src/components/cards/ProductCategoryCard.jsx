00cce6bdf02e6c0d20c9c4364333be05ffcabde9
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-13 00:00:01 +0530

    image

--- client/src/components/cards/ProductCategoryCard.jsx
+++ client/src/components/cards/ProductCategoryCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import image from './products.json';
+
 
 const Card = styled.div`
   width: 250px;
